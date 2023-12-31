0f9307f4e93edf5d15405889011f2a84ad7f9665
Alarick Whitted <awhitted834@gmail.com>
2023-12-31 11:16:55 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function superbowlWin(record) {
+    const win = record.find(entry => entry.result === "W");
+    if (win) {
+        return win.year;
+    } else {
+        return undefined; // or any other value you'd like to return if there's no win
+    }
+}
+
+// Example usage:
+const record = [
+    { year: '2015', result: "W" },
+    { year: '2014', result: "N/A" },
+    { year: '2013', result: "L" },
+    // ... other records
+];
+
+const winningYear = superbowlWin(record);
+console.log(winningYear); // Outputs: '2015' if there's a win, otherwise undefined
